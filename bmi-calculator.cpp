de24e91a9f0463927ac3c19997d1d402d3a843f1
AdwaithAji <adwaithaji07@gmail.com>
2026-03-04 14:58:10 +0530

    Initial commit of BMI app

---
+++ bmi-calculator.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+void operation(int &choice,bool &programExit);
+void menu(int &choice);
+void bmiMenu();
+int main(){
+
+    bool programExit = false;
+    int choice;
+
+    while(!programExit){
+        menu(choice);
+        operation(choice,programExit);
+     }
+     std::cout<<"You Have Exited The Program";   
+     return 0;
+
+
+}
+void operation(int &choice,bool &programExit){
+
+    switch(choice){
+        case 0: programExit = true; break;
+        case 1: bmiMenu();
+    }
+}
+void menu(int &choice){
+    std::cout<<"********************************"<<'\n';
+    std::cout<<"**********BMI-CALCULATOR********"<<'\n';
+    std::cout<<"0.Exit: "<<'\n';
+    std::cout<<"1.Check Bmi: "<<'\n';
+    std::cin>>choice;
+}
+void bmiMenu(){
+    float weight = 0;
+    float height = 0;
+    float bmi = 0;
+
+    std::cout<<"Enter your Weigth: ";
+    std::cin>>weight;
+    std::cout<<"Enter your height: ";
+    std::cin>>height;
+    if(height<=0){
+        std::cout<<"Height must be greater than 0:"<<'\n';
+        return;
+    }
+    bmi = weight/(height*height);
+    std::cout<<"BMI: "<< bmi<<'\n';
+    std::cout<<"-----------"<<'\n';
+    if(bmi < 18.5) 
+    std::cout << "Category: Underweight\n";
+    else if(bmi < 24.9) 
+    std::cout << "Category: Normal weight\n";
+    else if(bmi < 29.9) 
+    std::cout << "Category: Overweight\n";
+    else 
+    std::cout << "Category: Obese\n";
+}
